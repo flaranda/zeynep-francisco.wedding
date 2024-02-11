c27e45aefc9247978c2404f1d20b7a1ebf1f171d
Francisco Aranda <flarandagarcia@gmail.com>
2024-02-11 12:41:57 +0300

    🔧 (i18n): add i18n configuration

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-i18next').UserConfig} */
+
+module.exports = {
+  i18n: {
+    locales: ["es", "tr", "en"],
+    defaultLocale: "es",
+  },
+  defaultNS: false,
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+import i18next from "./next-i18next.config.js";
+
 const nextConfig = {
   reactStrictMode: true,
+  i18n: i18next.i18n,
 };
 
 export default nextConfig;
