ebcc64c2996b4d1d886849d91af9ae8d9b3d2307
Francisco Aranda <flarandagarcia@gmail.com>
2024-02-12 16:50:02 +0300

    🐛 (app): move Analytics component from document to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default appWithTranslation(App);
